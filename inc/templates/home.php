f9bf487ff2af096fc929e69680052fbeaf0ab0d7
vvye <ekaiser.hellwege@gmail.com>
2017-03-18 15:00:56 +0100

    Add basic two-column grid system

--- inc/templates/home.php
+++ inc/templates/home.php
@@ -1,12 +1,20 @@
-<h1>simpson</h1>
-<p>a <em>simp</em>le <em>soc</em>ial <em>n</em>etwork</p>
+<div class="grid">
 
-<h2>Hello World!</h2>
-<p>
-	Est velox luna, cesaris. Cum torquis tolerare, omnes tumultumquees consumere camerarius, domesticus nixes. Finiss
-	mori in divio!
-</p>
-<p>
-	Est velox luna, cesaris. Cum torquis tolerare, omnes tumultumquees consumere camerarius, domesticus nixes. Finiss
-	mori in divio!
-</p>
+	<div class="column">
+		<div class="content">
+			<h1>simpson</h1>
+			<p>a <em>simp</em>le <em>soc</em>ial <em>n</em>etwork</p>
+			<p>Hello World! <em>simpson</em> is a minimalist social network that exists for no reason in particular.</p>
+		</div>
+	</div>
+
+	<div class="column">
+		<div class="content">
+			<div class="panel">
+				right column
+			</div>
+		</div>
+	</div>
+
+</div>
+
