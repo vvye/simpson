395f0c329e19119ddc4ee51b1b75ef0be79c8384
vvye <ekaiser.hellwege@gmail.com>
2017-03-18 13:37:17 +0100

    Add heading to menu

--- inc/templates/home.php
+++ inc/templates/home.php
@@ -1,4 +1,4 @@
-<h1>Hello World!</h1>
+<h2>Hello World!</h2>
 <p>
 	Est velox luna, cesaris. Cum torquis tolerare, omnes tumultumquees consumere camerarius, domesticus nixes. Finiss
 	mori in divio!
--- inc/templates/menu.php
+++ inc/templates/menu.php
@@ -1,5 +1,6 @@
 <nav>
 	<ul>
+		<li><h1>simpson</h1></li>
 		<?php foreach ($menuItems as $menuItem): ?>
 			<li><a href="<?= $menuItem['name'] ?>"><?= $menuItem['caption'] ?? $menuItem['name'] ?></a></li>
 		<?php endforeach ?>
