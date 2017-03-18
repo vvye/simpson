640c8dfd7d661f8f4c8c9abf48de93bd6897eee1
vvye <ekaiser.hellwege@gmail.com>
2017-03-18 15:53:18 +0100

    Add & style register form

--- inc/templates/home.php
+++ inc/templates/home.php
@@ -6,9 +6,9 @@
 			<p>a <em>simp</em>le <em>soc</em>ial <em>n</em>etwork</p>
 			<p>Hello World! <em>simpson</em> is a minimalist social network that exists for no reason in particular.</p>
 			<form>
-				<label><input type="email" id="email" name="email" placeholder="E-Mail" /></label>
-				<label><input type="password" id="password" name="password" placeholder="password" /></label>
-				<button type="submit">Log in</button>
+				<label><input type="email" name="email" placeholder="E-Mail" /></label>
+				<label><input type="password" name="password" placeholder="Password" /></label>
+				<button type="submit" name="login">Log in</button>
 			</form>
 		</div>
 	</div>
@@ -17,6 +17,31 @@
 		<div class="content">
 			<div class="panel register">
 				<h3>Join the fun!</h3>
+				<form>
+					<table>
+						<tr>
+							<td><label for="first-name">First name:</label></td>
+							<td><input type="text" id="first-name" name="first-name" /></td>
+						</tr>
+						<tr>
+							<td><label for="last-name">Last name:</label></td>
+							<td><input type="text" id="last-name" name="last-name" /></td>
+						</tr>
+						<tr>
+							<td><label for="email">E-Mail:</label></td>
+							<td><input type="email" id="email" name="email" /></td>
+						</tr>
+						<tr>
+							<td><label for="password">Choose a password:</label></td>
+							<td><input type="password" id="password" name="password" /></td>
+						</tr>
+						<tr>
+							<td colspan="2" class="action">
+								<button type="submit" class="large primary full-size">Join</button>
+							</td>
+						</tr>
+					</table>
+				</form>
 			</div>
 		</div>
 	</div>
