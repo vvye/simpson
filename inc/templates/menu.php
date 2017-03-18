faf8efe09c35bbdada746c2d20e1b51d4be95dad
vvye <ekaiser.hellwege@gmail.com>
2017-03-18 14:44:55 +0100

    Change heading in menu from h1 to span

--- inc/templates/menu.php
+++ inc/templates/menu.php
@@ -1,6 +1,6 @@
 <nav>
 	<ul>
-		<li><h1>simpson</h1></li>
+		<li><span>simpson</span></li>
 		<?php foreach ($menuItems as $menuItem): ?>
 			<li><a href="?p=<?= $menuItem['name'] ?>"><?= $menuItem['caption'] ?? $menuItem['name'] ?></a></li>
 		<?php endforeach ?>
