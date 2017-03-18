6a0d8a3cb66c1b2ceb64a5aefaaafbe0f76a973b
vvye <ekaiser.hellwege@gmail.com>
2017-03-18 15:53:47 +0100

    Make font lighter

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<title>simpson</title>
-		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,400i,700" />
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather+Sans:300,300i,700" />
 		<link rel="stylesheet" type="text/css" href="css/main.css" />
 		<link rel="stylesheet" type="text/css" href="css/form.css" />
 	</head>
