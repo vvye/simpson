54c8d5293275725a5d0d4434e9e65e54362cdd94
vvye <ekaiser.hellwege@gmail.com>
2017-03-18 15:07:48 +0100

    Change font

--- index.php
+++ index.php
@@ -17,7 +17,8 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<title>simpson</title>
-		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<link rel="stylesheet" type="text/css" href="css/main.css" />
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,400i,700" />
 	</head>
 	<body>
 
