31460d8556461b1094e1773753f0a39b877c0890
vvye <ekaiser.hellwege@gmail.com>
2017-03-18 14:50:28 +0100

    Add title on home page

--- inc/templates/home.php
+++ inc/templates/home.php
@@ -1,3 +1,6 @@
+<h1>simpson</h1>
+<p>a <em>simp</em>le <em>soc</em>ial <em>n</em>etwork</p>
+
 <h2>Hello World!</h2>
 <p>
 	Est velox luna, cesaris. Cum torquis tolerare, omnes tumultumquees consumere camerarius, domesticus nixes. Finiss
