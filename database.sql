4e0e457095eb8c1a1fc3ab5b5ca13403360e767b
vvye <ekaiser.hellwege@gmail.com>
2018-08-07 22:38:17 +0200

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,11 +1,12 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
 
 CREATE TABLE `messages` (
   `id` int(11) NOT NULL,
@@ -14,42 +15,32 @@ CREATE TABLE `messages` (
   `text` longtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `replies` (
-  `id` int(11) NOT NULL,
-  `parent` int(11) NOT NULL,
-  `user` int(11) NOT NULL,
-  `time` int(11) NOT NULL,
-  `text` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `first_name` varchar(100) NOT NULL,
   `last_name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `password_hash` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
   `email_public` tinyint(4) NOT NULL DEFAULT '0',
   `bio` longtext NOT NULL,
   `registration_time` int(11) NOT NULL,
   `last_login_time` int(11) NOT NULL,
   `admin` tinyint(4) NOT NULL DEFAULT '0',
-  `banned` tinyint(4) NOT NULL DEFAULT '0'
+  `banned` tinyint(4) NOT NULL DEFAULT '0',
+  `csrf_token` varchar(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 ALTER TABLE `messages`
   ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `replies`
-  ADD PRIMARY KEY (`id`);
-
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
-
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `id` (`id`);
 
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
