464e71c8938e32da9219d589ac83c6fa6292fc70
vvye <ekaiser.hellwege@gmail.com>
2018-08-09 22:09:45 +0200

    Remove unnecessary labels

--- inc/templates/edit-profile.php
+++ inc/templates/edit-profile.php
@@ -5,19 +5,19 @@
 			<tr>
 				<td><label for="first-name">First name:</label></td>
 				<td>
-					<label><input type="text" id="first-name" name="first-name" /></label>
+					<input type="text" id="first-name" name="first-name" />
 				</td>
 			</tr>
 			<tr>
 				<td><label for="last-name">Last name:</label></td>
 				<td>
-					<label><input type="text" id="last-name" name="last-name" /></label>
+					<input type="text" id="last-name" name="last-name" />
 				</td>
 			</tr>
 			<tr>
 				<td><label for="email">E-Mail address:</label></td>
 				<td>
-					<label><input type="email" id="email" name="email" /></label>
+					<input type="email" id="email" name="email" />
 					<label class="checkbox">
 						<input type="checkbox" id="email-public" name="email-public" />
 						<span class="label">Show my e-mail address in public</span>
@@ -27,7 +27,7 @@
 			<tr>
 				<td><label for="bio">Bio:</label></td>
 				<td>
-					<label><textarea id="bio" name="bio"></textarea></label>
+					<textarea id="bio" name="bio"></textarea>
 				</td>
 			</tr>
 		</table>
@@ -37,12 +37,16 @@
 		<table>
 			<tr>
 				<td><label for="old-password">Old password:</label></td>
-				<td><label><input type="password" id="old-password" name="old-password" /></label></td>
+				<td><input type="password" id="old-password" name="old-password" /></td>
 			</tr>
 			<tr>
 				<td><label for="new-password-1">New password:</label></td>
-				<td><label><input type="password" id="new-password-1" name="new-password-1" /></label></td>
-				<td><label><input type="password" id="new-password-2" name="new-password-2" /></label></td>
+				<td>
+					<input type="password" class="stacked" id="new-password-1" name="new-password-1" />
+				</td>
+				<td>
+					<input type="password" class="stacked" id="new-password-2" name="new-password-2" />
+				</td>
 			</tr>
 		</table>
 	</fieldset>
