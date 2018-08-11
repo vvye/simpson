b2d6da27e8965094f97bb740929c2ea38c52e64f
vvye <ekaiser.hellwege@gmail.com>
2018-08-11 16:30:41 +0200

    Update message mockup

--- inc/templates/messages.php
+++ inc/templates/messages.php
@@ -29,6 +29,131 @@
 		Nunquam imitari musa. Cum accola velum, omnes bursaes amor domesticus, flavum adgiumes. Est regius elevatus,
 		cesaris. Lumen moris, tanquam altus devirginato. Bullas ridetis in hafnia! Candidatuss favere! Cum racana
 		assimilant, omnes eleateses aperto festus, azureus messores.
+		<div class="replies">
+			<h3>3 replies</h3>
+			<ul>
+				<li class="reply">
+					<img class="avatar" src="<?= BASE_PATH ?>/img/avatars/default.png" />
+					<div class="content">
+						<div class="info">
+							<h4>name</h4>
+							<span class="post-time">odifhgd odijgfoidjfg</span>
+						</div>
+						Nunquam imitari musa. Cum accola velum, omnes bursaes amor domesticus, flavum adgiumes. Est
+						regius elevatus,
+						cesaris. Lumen moris, tanquam altus devirginato. Bullas ridetis in hafnia! Candidatuss favere!
+						Cum racana
+						assimilant, omnes eleateses aperto festus, azureus messores.
+					</div>
+					<div class="clearfix"></div>
+				</li>
+				<li class="reply">
+					<img class="avatar" src="<?= BASE_PATH ?>/img/avatars/default.png" />
+					<div class="content">
+						<div class="info">
+							<h4>name</h4>
+							<span class="post-time">odifhgd odijgfoidjfg</span>
+						</div>
+						Nunquam imitari musa. Cum accola velum, omnes bursaes amor domesticus, flavum adgiumes. Est
+						regius elevatus,
+						cesaris. Lumen moris, tanquam altus devirginato. Bullas ridetis in hafnia! Candidatuss favere!
+						Cum racana
+						assimilant, omnes eleateses aperto festus, azureus messores.
+						Nunquam imitari musa. Cum accola velum, omnes bursaes amor domesticus, flavum adgiumes. Est
+						regius elevatus,
+						cesaris. Lumen moris, tanquam altus devirginato. Bullas ridetis in hafnia! Candidatuss favere!
+						Cum racana
+						assimilant, omnes eleateses aperto festus, azureus messores.
+						Nunquam imitari musa. Cum accola velum, omnes bursaes amor domesticus, flavum adgiumes. Est
+						regius elevatus,
+						cesaris. Lumen moris, tanquam altus devirginato. Bullas ridetis in hafnia! Candidatuss favere!
+						Cum racana
+						assimilant, omnes eleateses aperto festus, azureus messores.
+					</div>
+					<div class="clearfix"></div>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="clearfix"></div>
+</div>
+
+<div class="message panel">
+	<img class="avatar" src="<?= BASE_PATH ?>/img/avatars/default.png" />
+	<div class="content">
+		<div class="info">
+			<h3>name</h3>
+			<h3>► name2</h3>
+			<span class="post-time">odifhgd odijgfoidjfg</span>
+		</div>
+		Nunquam imitari musa. Cum accola velum, omnes bursaes amor domesticus, flavum adgiumes. Est regius elevatus,
+		cesaris. Lumen moris, tanquam altus devirginato. Bullas ridetis in hafnia! Candidatuss favere! Cum racana
+		assimilant, omnes eleateses aperto festus, azureus messores.
+
+		Nunquam imitari musa. Cum accola velum, omnes bursaes amor domesticus, flavum adgiumes. Est regius elevatus,
+		cesaris. Lumen moris, tanquam altus devirginato. Bullas ridetis in hafnia! Candidatuss favere! Cum racana
+		assimilant, omnes eleateses aperto festus, azureus messores.
+
+		Nunquam imitari musa. Cum accola velum, omnes bursaes amor domesticus, flavum adgiumes. Est regius elevatus,
+		cesaris. Lumen moris, tanquam altus devirginato. Bullas ridetis in hafnia! Candidatuss favere! Cum racana
+		assimilant, omnes eleateses aperto festus, azureus messores.
+
+		Nunquam imitari musa. Cum accola velum, omnes bursaes amor domesticus, flavum adgiumes. Est regius elevatus,
+		cesaris. Lumen moris, tanquam altus devirginato. Bullas ridetis in hafnia! Candidatuss favere! Cum racana
+		assimilant, omnes eleateses aperto festus, azureus messores.
+
+		Nunquam imitari musa. Cum accola velum, omnes bursaes amor domesticus, flavum adgiumes. Est regius elevatus,
+		cesaris. Lumen moris, tanquam altus devirginato. Bullas ridetis in hafnia! Candidatuss favere! Cum racana
+		assimilant, omnes eleateses aperto festus, azureus messores.
+
+		Nunquam imitari musa. Cum accola velum, omnes bursaes amor domesticus, flavum adgiumes. Est regius elevatus,
+		cesaris. Lumen moris, tanquam altus devirginato. Bullas ridetis in hafnia! Candidatuss favere! Cum racana
+		assimilant, omnes eleateses aperto festus, azureus messores.
+		<div class="replies">
+			<h3>3 replies</h3>
+			<ul>
+				<li class="reply">
+					<img class="avatar" src="<?= BASE_PATH ?>/img/avatars/default.png" />
+					<div class="content">
+						<div class="info">
+							<h4>name</h4>
+							<span class="post-time">odifhgd odijgfoidjfg</span>
+						</div>
+						Nunquam imitari musa. Cum accola velum, omnes bursaes amor domesticus, flavum adgiumes. Est
+						regius elevatus,
+						cesaris. Lumen moris, tanquam altus devirginato. Bullas ridetis in hafnia! Candidatuss favere!
+						Cum racana
+						assimilant, omnes eleateses aperto festus, azureus messores.
+					</div>
+					<div class="clearfix"></div>
+				</li>
+				<li class="reply">
+					<img class="avatar" src="<?= BASE_PATH ?>/img/avatars/default.png" />
+					<div class="content">
+						<div class="info">
+							<h4>name</h4>
+							<span class="post-time">odifhgd odijgfoidjfg</span>
+						</div>
+						Nunquam imitari musa. Cum accola velum, omnes bursaes amor domesticus, flavum adgiumes. Est
+						regius elevatus,
+						cesaris. Lumen moris, tanquam altus devirginato. Bullas ridetis in hafnia! Candidatuss favere!
+						Cum racana
+						assimilant, omnes eleateses aperto festus, azureus messores.
+						Nunquam imitari musa. Cum accola velum, omnes bursaes amor domesticus, flavum adgiumes. Est
+						regius elevatus,
+						cesaris. Lumen moris, tanquam altus devirginato. Bullas ridetis in hafnia! Candidatuss favere!
+						Cum racana
+						assimilant, omnes eleateses aperto festus, azureus messores.
+						Nunquam imitari musa. Cum accola velum, omnes bursaes amor domesticus, flavum adgiumes. Est
+						regius elevatus,
+						cesaris. Lumen moris, tanquam altus devirginato. Bullas ridetis in hafnia! Candidatuss favere!
+						Cum racana
+						assimilant, omnes eleateses aperto festus, azureus messores.
+					</div>
+					<div class="clearfix"></div>
+				</li>
+			</ul>
+		</div>
 	</div>
 	<div class="clearfix"></div>
 </div>
